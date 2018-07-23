9f0590051958830baa8608e99cc4a3c344f57838
Nicholas Powers <njpowers7915@gmail.com>
2018-07-23 16:51:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 function deleteFromDriverByKey(driver, key) {
   let clone = { ...driver};
   delete clone[key];
+  return clone;
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
