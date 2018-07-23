2cc8c285941aee756d010df47eb3cee59e0a0325
Nicholas Powers <njpowers7915@gmail.com>
2018-07-23 16:41:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  return Object.assign(driver, {key: value});
+  driver[key] = value;
+  return driver;
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
